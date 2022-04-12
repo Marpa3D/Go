2a4dc46dcf3e8277b6f6c531afff6482db7e5e6a
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2022-04-12 20:05:59 +0300

    Создан шаблон приложение на Fyne

---
+++ Fyne/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
+func main(){
+	a := app.New()
+	w := a.NewWindow("Первое оконное приложение на GO!!!")
+	w.SetContent(widget.NewLabel("Приветики!"))
+	w.ShowAndRun()
+
+}
