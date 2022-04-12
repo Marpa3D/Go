f88e823e5adc15e0bfca7ba230c2bca35e0edfb7
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2022-04-12 20:34:41 +0300

    Самые крутые часы на планете!)))
    Также добавлена папка для скриншотов приложений.
    Добавлен скрин работы часов в GUI
    от Fyne

--- Fyne/hello.go
+++ Fyne/hello.go
@@ -5,11 +5,27 @@ package main
 import (
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/widget"
+	"time"
 )
+func updTime(clock *widget.Label){
+	formatTime := time.Now().Format("Текущее время: 03:04:05")
+	clock.SetText(formatTime)
+}
+
 func main(){
 	a := app.New()
-	w := a.NewWindow("Первое оконное приложение на GO!!!")
-	w.SetContent(widget.NewLabel("Приветики!"))
+	w := a.NewWindow("Крутые часы")
+
+	clock := widget.NewLabel(" ")
+	updTime(clock)
+
+	w.SetContent(clock)
+
+	go func(){
+		for range time.Tick(time.Second){
+			updTime(clock)
+		}
+	}()
 	w.ShowAndRun()
 
 }
