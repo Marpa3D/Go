cd9cd3f269915b02b5bc7ae55d47fce42b178175
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2022-05-20 09:48:17 +0300

    Код загрузки html шаблонов

--- partyinvites/main.go
+++ partyinvites/main.go
@@ -1,13 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+)
 
 type Rsvp struct {
 	Name, Email, Phone string
 	WillAtteend bool
 }
 var response = make([]*Rsvp, 0, 10)
+var templates =make(map[string]*template.Template, 3)
+
+func loadTemlates()  {
+	templateNames = [5]string{"welcome", "form", "sanks", "sorry", "list"}
+	for index, name = range templates{
+		t, err = template.ParseFiles("layuot.html", name + ".html")
+		if err == nil{
+			templates[name] = t
+			fmt.Println("Загружен шаблон", index, name)
+		} else {
+			panic(err)
+		}
+	}
+}
 
 func main()  {
 	fmt.Println("Поехали!")
+	loadTemlates()
 }
