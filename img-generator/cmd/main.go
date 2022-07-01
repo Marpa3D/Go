3ec48a508e43d4832660179a35f49b3d84d5947a
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2022-07-01 13:44:12 +0300

    Создал сервер и основные обработчики

--- img-generator/cmd/main.go
+++ img-generator/cmd/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "img-generator/internal/server"
+
 func main() {
 	server.Run()
 }
--- img-generator/internal/server/server.go
+++ img-generator/internal/server/server.go
@@ -1,11 +1,40 @@
 package server
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
+
+func rend(w http.ResponseWriter, msg string) {
+	_, err := w.Write([]byte(msg))
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func imgHandler(w http.ResponseWriter, r *http.Request) {
+	rend(w, "img")
+}
+
+func faviconHandler(w http.ResponseWriter, r *http.Request) {
+	rend(w, "favicon")
+}
+
+func pingHandler(w http.ResponseWriter, r *http.Request) {
+	rend(w, "PONG")
+}
+
+func robotsHandler(w http.ResponseWriter, r *http.Request) {
+	rend(w, "robots")
+}
 
 func Run() {
 	http.HandleFunc("/", imgHandler)
 	http.HandleFunc("/favicon", faviconHandler)
 	http.HandleFunc("/ping", pingHandler)
 	http.HandleFunc("/robots.txt", robotsHandler)
-	http.ListenAndServe(":8080", nil)
+	log.Print("Сервер стартует...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
