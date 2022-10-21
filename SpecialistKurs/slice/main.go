5a8b8379e3755460af7f2a629870c3a0e7865b5e
Viacheslav "Marpa3D" Sharov <genesis@marpa.su>
2022-10-21 13:40:30 +0300

    Способы создания среза (слайса)

--- SpecialistKurs/slice/main.go
+++ SpecialistKurs/slice/main.go
@@ -4,15 +4,26 @@ package main
 import "fmt"
 
 func main() {
-	slice := []int{10, 20, 30, 40}
-	slice[0] *= 10
-	slice[2] = 300
-	fmt.Println("Slice value:", slice, len(slice), cap(slice))
-	slice = append(slice, 20) // Добавление нового элемента
-	fmt.Println("Slice value:", slice, len(slice), cap(slice))
 
-	// итерирование по срезу
-	for i, v := range slice {
-		fmt.Println(i, v)
-	}
+	// Срез (он же слайс) - это динамическая обвязка над массивом. Это фрагмент. диапазон (область) массива.
+	//slice := []int{10, 20, 30, 40}
+	//slice[0] *= 10
+	//slice[2] = 300
+	//fmt.Println("Slice value:", slice, len(slice), cap(slice))
+	//slice = append(slice, 20) // Добавление нового элемента
+	//fmt.Println("Slice value:", slice, len(slice), cap(slice))
+	//
+	//// итерирование по срезу
+	//for i, v := range slice {
+	//	fmt.Println(i, v)
+	//}
+
+	startArr := [5]int{10, 20, 30, 40, 50}
+	var startSlice []int = startArr[0:3] // Срез инициализируется пустыми квадратными скобками
+	fmt.Println(startSlice)
+	// 1) Создали срез на уже существующем массиве startArr
+
+	// 2) Создание среза (слайса) без явной инициализации массива
+	secondSlice := []int{10, 20, 30, 40, 50}
+	fmt.Println("Second slice:", secondSlice)
 }
