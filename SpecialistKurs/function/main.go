708b9179959b4461a96639468efdd761243de88b
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2022-11-21 14:52:53 +0300

    Чтение аргументов командной строки. Их сложение и умножение

---
+++ SpecialistKurs/function/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	n1, _ := strconv.Atoi(os.Args[1])
+	n2, _ := strconv.Atoi(os.Args[2])
+	res1 := n1 + n2
+	res2 := n1 * n2
+	fmt.Println("Сумма аргументов:", res1, "Произведение аргументов:", res2)
+}
