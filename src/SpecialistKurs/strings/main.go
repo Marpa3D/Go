4221f05485f7c103de44336a039902f205649b5e
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2022-12-03 17:24:06 +0300

    ПÑПрактика. Ð Ðпо string. Продолжение

--- src/SpecialistKurs/strings/main.go
+++ src/SpecialistKurs/strings/main.go
@@ -5,7 +5,7 @@ import "fmt"
 
 func main() {
 	// Создание строки через краткую нотацию
-	str := "Hello! Это строка на кирилице!"
+	str := "Hello! Это строка на кирилице!" // И string не изменяема
 	fmt.Println(str)
 	fmt.Printf("Тип данных: %T\n", str)
 
@@ -13,7 +13,8 @@ func main() {
 	bs := []byte(str)
 	fmt.Println(bs)                    // выводит коды символов из UTF - 8
 	fmt.Printf("Тип данных: %T\n", bs) // type []uint8
-
+	bs = append([]byte(str[:2]), 'O', 'O', 'U')
+	fmt.Printf("Добавлен символ: %q\n  для строки %s", bs, str)
 	// Итерация по строке
 	for i := 0; i < len(str); i++ {
 		fmt.Printf("%#U\n ", str[i])
