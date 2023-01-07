29c499bfc01cb64284d57b56b5a26685b2f7464d
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2023-01-07 15:49:13 +0300

    Добавлена функция подсчета гланых в строке

--- src/SpecialistKurs/codeWars/main.go
+++ src/SpecialistKurs/codeWars/main.go
@@ -1,4 +1,5 @@
 // Пакет strings - вставка пробелов между символами строки
+// Подсчет гласных во входящей строке
 package main
 
 import (
@@ -11,4 +12,18 @@ func main() {
 	s := "Hello WORLD!!!"
 	v := strings.SplitAfter(s, "")
 	fmt.Println(v)
+
+	fmt.Println(GetCount("hello dolly and Jhon!"))
+
+}
+
+func GetCount(str string) (count int) {
+	// Enter solution here
+	count = 0
+	for i, _ := range str {
+		if str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' {
+			count++
+		}
+	}
+	return count
 }
