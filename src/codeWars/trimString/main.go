387d84fea96872ddeb147824c607d8b2f41609f6
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2023-01-12 08:40:12 +0300

    Привякзка проверки длины строки

--- src/codeWars/trimString/main.go
+++ src/codeWars/trimString/main.go
@@ -5,16 +5,22 @@ import "fmt"
 
 func main() {
 	var text string
-	var width int
+	var width int = 8
 	fmt.Scanf("%s %d", &text, &width)
-	t := []byte(text)
-	//t = t[:width]
-	t = append(t[:width], '.', '.', '.')
-	res := string(t)
-	// Возьмите первые `width` байт строки `text`,
-	// допишите в конце `...` и сохраните результат
-	// в переменную `res`
-	// ...
 
-	fmt.Println(res)
+	if len(text) <= width {
+		fmt.Println(text)
+	} else {
+		t := []byte(text)
+
+		t = append(t[:width], '.', '.', '.')
+		res := string(t)
+		// Возьмите первые `width` байт строки `text`,
+		// допишите в конце `...` и сохраните результат
+		// в переменную `res`
+		// ...
+
+		fmt.Println(res)
+
+	}
 }
