8f576f9c1be03667efae3c0e9d810db86aab1120
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2023-01-24 19:40:05 +0300

    раÐПрактика с пакетом "time"

--- src/derekBanasTime/main.go
+++ src/derekBanasTime/main.go
@@ -1,5 +1,24 @@
+// Работа со временем. Пакет "time"
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+var pl = fmt.Println
+
 func main() {
+	now := time.Now()
+	pl(now.Day(), now.Month(), now.Year())
+	pl(now.Hour(), now.Minute(), now.Second())
+
+	// Генерация случайного числа
+	tNow := time.Now().Unix()
+	rand.Seed(tNow)
+	randNum := rand.Intn(80) + 1
+	pl(randNum)
+	fmt.Printf("%.f", 3.1498)
 
 }
