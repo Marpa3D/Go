a77c4b158f98f252bea07dfc37b1a8162726f158
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2023-01-25 19:34:40 +0300

    РЕализована функция Pop и проверка стека

--- src/dataStructs/stack/main.go
+++ src/dataStructs/stack/main.go
@@ -12,11 +12,32 @@ func (s *Stack) Push(data int) {
 	s.items = append(s.items, data)
 }
 
+// Реализация функции взятия из стека. Pop
+func (s *Stack) Pop() {
+	if s.IsEmpty() {
+		return
+	}
+	s.items = s.items[:len(s.items)-1]
+}
+
+func (s *Stack) IsEmpty() bool {
+	if len(s.items) == 0 {
+		return true
+	}
+	return false
+}
+
 func main() {
 	var st Stack
 	st.Push(10)
 	fmt.Printf("%+v\t adress: %p\t Длинна в байтах: %d\n", st, &st, len(st.items))
 
 	st.Push(20)
+	st.Push(30)
+	st.Push(40)
+
+	fmt.Printf("%+v\t adress: %p\t Длинна в байтах: %d\n", st, &st, len(st.items))
+
+	st.Pop()
 	fmt.Printf("%+v\t adress: %p\t Длинна в байтах: %d\n", st, &st, len(st.items))
 }
