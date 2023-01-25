5c81a630ba36215e23891487c9c6253e8c9dcc1b
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2023-01-25 19:23:22 +0300

    Реализована функция Push

--- src/dataStructs/stack/main.go
+++ src/dataStructs/stack/main.go
@@ -1,6 +1,22 @@
 // Структуры данных. Стек
 package main
 
-func main() {
+import "fmt"
 
+type Stack struct {
+	items []int
+}
+
+// Реализация функции добавления в стек. Push
+func (s *Stack) Push(data int) {
+	s.items = append(s.items, data)
+}
+
+func main() {
+	var st Stack
+	st.Push(10)
+	fmt.Printf("%+v\t adress: %p\t Длинна в байтах: %d\n", st, &st, len(st.items))
+
+	st.Push(20)
+	fmt.Printf("%+v\t adress: %p\t Длинна в байтах: %d\n", st, &st, len(st.items))
 }
