fc35f23910a17d4fb32fd2cf5c876290e0415c97
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2023-01-25 19:54:30 +0300

    Реализация функции Print() - выводит спиÑвсе, итерация по стеку и печать элем элементов

--- src/dataStructs/stack/main.go
+++ src/dataStructs/stack/main.go
@@ -7,12 +7,12 @@ type Stack struct {
 	items []int
 }
 
-// Реализация функции добавления в стек. Push
+// Push  - реализация функции добавления в стек
 func (s *Stack) Push(data int) {
 	s.items = append(s.items, data)
 }
 
-// Реализация функции взятия из стека. Pop
+// Pop - реализация функции взятия из стека
 func (s *Stack) Pop() {
 	if s.IsEmpty() {
 		return
@@ -20,7 +20,7 @@ func (s *Stack) Pop() {
 	s.items = s.items[:len(s.items)-1]
 }
 
-// Проверка стека на наличие данных
+// IsEmpty - проверка стека на наличие данных
 func (s *Stack) IsEmpty() bool {
 	if len(s.items) == 0 {
 		return true
@@ -34,6 +34,14 @@ func (s *Stack) Top() (int, error) {
 		return 0, fmt.Errorf("Стек пустой!")
 	}
 	return s.items[len(s.items)-1], nil
+}
+
+// Print - итерация по стеку и печать элементов в нем
+func (s *Stack) Print() {
+	for _, val := range s.items {
+		fmt.Print(val, " ")
+	}
+	fmt.Println()
 }
 
 func main() {
@@ -52,4 +60,6 @@ func main() {
 
 	st.Top()
 	fmt.Println(st.Top())
+
+	st.Print() // Выводит все, что есть в стеке
 }
