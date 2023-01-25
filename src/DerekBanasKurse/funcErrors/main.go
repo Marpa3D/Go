d882629a4a6061b3fad540f9c8820254a241052d
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2023-01-25 11:55:40 +0300

    Вариационная функцияÑ. НеиЛюбое колличество аргументов при Ð функции

--- src/DerekBanasKurse/funcErrors/main.go
+++ src/DerekBanasKurse/funcErrors/main.go
@@ -5,13 +5,26 @@ import "fmt"
 
 func getDiv(x, y float64) (ans float64, err error) {
 	if y == 0 {
-		return 0, fmt.Errorf("Вы не можете делить на ноль!")
+		return 0, fmt.Errorf("Вы не можете делить на ноль!\n")
 	} else {
 		return x / y, nil
 	}
 }
 
+// Вариационная функция
+func getVariaticSum(num ...int) int {
+	var sum int
+	for _, val := range num {
+		sum += val
+		fmt.Printf("%d\t", sum)
+	}
+	fmt.Println("\n")
+	return sum
+}
+
 func main() {
 	fmt.Println(getDiv(14, 0))
+
+	fmt.Println("Общая сумма аргументов: ", getVariaticSum(1, 2, 3, 4, 7, 8))
 
 }
