bc9565559d15d6d9785515088a3209186a1d2213
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2023-01-27 07:52:33 +0300

    Реализация команды echo в Unix

--- src/echoUnix/main.go
+++ src/echoUnix/main.go
@@ -1,6 +1,21 @@
 // Реализация команды echo в Unix
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
+	var s, space string
+	for i := 1; i < len(os.Args); i++ {
+		s += space + os.Args[i]
+		space = " "
+	}
+	if len(os.Args) == 1 {
+		fmt.Println("Вы не добавлили аргументы в строке!)\nПерезапустите программу.")
+	} else {
+		fmt.Println("Аргументы командной строки:", s)
+	}
 
 }
