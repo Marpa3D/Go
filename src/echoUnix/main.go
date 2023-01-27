5b7f4f45bfef509514ffa1fcbd33a864f850a8d5
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2023-01-27 08:08:32 +0300

    ÐФорматированный вывод комÐнаналога °нды Echo

--- src/echoUnix/main.go
+++ src/echoUnix/main.go
@@ -7,10 +7,11 @@ import (
 )
 
 func main() {
-	var s, space string
-	for i := 1; i < len(os.Args); i++ {
-		s += space + os.Args[i]
+	s, space := "", ""
+	for i, arg := range os.Args[:] {
+		s += space + arg
 		space = " "
+		fmt.Printf("Индекс строки: [%d]  Аргумент строки: %s\n", i, arg)
 	}
 	if len(os.Args) == 1 {
 		fmt.Println("Вы не добавлили аргументы в строке!)\nПерезапустите программу.")
