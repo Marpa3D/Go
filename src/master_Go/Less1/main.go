fb6d36d54185bcf6da47971c1ee38206d3a52ba9
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2023-01-30 13:33:24 +0300

    Команды сборки под разныÐпрограммы go µ платформы

--- src/master_Go/Less1/main.go
+++ src/master_Go/Less1/main.go
@@ -8,6 +8,10 @@ import (
 
 func main() {
 
+	// GOOS=windows GOARCH=amd64 go build -o winapp.exe Компиляция под Windows
+	// GOOS=linux GOARCH=amd64 go build -o Компиляция под Linux
+	// GOOS=darwin GOARCH=amd64 go build -o Компиляция под Mac
+
 	go fmt.Println("Это горутина. Программа работает!)")
 	fmt.Println("Обычная инструкция)")
 
