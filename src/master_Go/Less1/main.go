7cb253a8bf69a641ce0df89a2db8522382211900
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2023-01-30 12:56:49 +0300

    Практика использования команды go [args] и горутины

--- src/master_Go/Less1/main.go
+++ src/master_Go/Less1/main.go
@@ -1,5 +1,15 @@
+// Практика использования команды go [args] и горутины
 package main
+
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 
+	go fmt.Println("Это горутина. Программа работает!)")
+	fmt.Println("Обычная инструкция)")
+
+	time.Sleep(2 * time.Second)
 }
