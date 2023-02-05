9a485186e6f225cace957e1e2e14f280e11ecb23
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2023-02-05 19:24:21 +0300

    АлкгÐАлгоритмыÑ. Линейный поиск O(n)

---
+++ src/master_Go/algoritms/linear_search/go.mod
@@ -0,0 +1,3 @@
+module master_Go/algoritms/linear_search
+
+go 1.18
---
+++ src/master_Go/algoritms/linear_search/main.go
@@ -0,0 +1,21 @@
+// Алгоритмы. Линейный поиск, O(n)
+package main
+
+import (
+	"fmt"
+)
+
+func linear_search(arr []int, target int) int {
+
+	for i, _ := range arr {
+		if arr[i] == target {
+			return i
+		}
+	}
+	return -1 // ВВозвращаемое значение в стиле псевдокода!)))
+}
+
+func main() {
+	a := []int{1, 4, 5, 7, 9, 8, 10}
+	fmt.Println(linear_search(a, 8))
+}
