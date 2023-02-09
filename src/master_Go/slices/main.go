c6187836b63d6c40c96de79f6fd35aeb515d40e9
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2023-02-09 09:40:45 +0300

    С литералом срезаÐ, с type и итерация по срещзÑу

--- src/master_Go/slices/main.go
+++ src/master_Go/slices/main.go
@@ -10,4 +10,27 @@ func main() {
 	//slice[1] = 18
 	//fmt.Println(slice) // panic : index out of range [1] length 0
 
+	// Literal slice
+	num := []float64{1.1, 2.4, 8.8, 18.0}
+	fmt.Println(num)
+
+	// с помощью встроенной функции make
+	numbers := make([]int, 10) // срез инициализирован 10-ю zero value for int: 0
+	fmt.Printf("%#v\n", numbers)
+
+	// в связке с произвольным type
+	type names []string
+	firstName := names{
+		"Mark",
+		"Ornella",
+		"Maria",
+		"July",
+	}
+	fmt.Println(firstName)
+
+	// Итерация с range
+	for index, value := range firstName {
+		fmt.Printf("Index: %+v Value: %+v\n", index, value)
+	}
+
 }
