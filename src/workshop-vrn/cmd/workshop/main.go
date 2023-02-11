a52e277e46895408ed18958e689147ff8ddfa53c
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2023-02-11 17:19:04 +0300

    Add router

--- src/workshop-vrn/cmd/workshop/main.go
+++ src/workshop-vrn/cmd/workshop/main.go
@@ -1,6 +1,23 @@
 // Web service with Iliya Kaznacheev
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+	
+	"github.com/go-chi/chi/v5"
 
+	"workshop-vrn/internal/handler"
+)
+
+func main() {
+	h := handler.NewHandler()
+	r := chi.NewRouter()
+	r.Get("/hello", h.Hello)
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Server starting. Port :8080")
 }
