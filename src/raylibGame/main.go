684b65fee82384d3cb0ffb9114adf355483fdd99
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2023-02-15 02:02:04 +0300

    Главный цикл и игры

--- src/raylibGame/main.go
+++ src/raylibGame/main.go
@@ -3,19 +3,57 @@ package main
 
 import rl "github.com/gen2brain/raylib-go/raylib"
 
-func main() {
-	rl.InitWindow(800, 450, "raylib [core] example - начальное окно")
+const (
+	screenWidth  = 1000
+	screenHeigth = 480
+)
+
+var (
+	running      = true
+	bgColor      = rl.NewColor(147, 211, 196, 255)
+	grassSprite  rl.Texture2D
+	playerSprite rl.Texture2D
+)
+
+func init() {
+	rl.InitWindow(screenWidth, screenHeigth, "Стартовый экран")
+	rl.SetExitKey(0) // окно не закроется по нажатию [esc]. Только мышью на [X]
+	grassSprite = rl.LoadTexture("resurses/Tilesets/groundTiles/oldTiles/Grass.png")
+	playerSprite = rl.LoadTexture("resurses/Characters/CharacterSpriteSheet.png")
 	rl.SetTargetFPS(60)
+}
 
-	for !rl.WindowShouldClose() {
-		rl.BeginDrawing()
+func drawScene() {
+	rl.DrawTexture(grassSprite, 100, 100, rl.White)
+	rl.DrawTexture(playerSprite, 200, 100, rl.White)
+}
 
-		rl.ClearBackground(rl.RayWhite)
+func input() {}
 
-		rl.DrawText("Welcome!!! This is first game window!", 190, 200, 20, rl.DarkGray)
+func update() {
+	running = !rl.WindowShouldClose()
+}
 
-		rl.EndDrawing()
+func render() {
+	rl.BeginDrawing()
+	rl.ClearBackground(bgColor)
+	drawScene()
+	rl.EndDrawing()
+}
+
+func quit() {
+	rl.UnloadTexture(grassSprite)
+	rl.UnloadTexture(playerSprite)
+	rl.CloseWindow()
+}
+
+func main() {
+
+	for running {
+		input()
+		update()
+		render()
 	}
 
-	rl.CloseWindow()
+	quit()
 }
