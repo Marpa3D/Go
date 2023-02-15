1b5e71254a7e3f2034283c93ee784d42bc0e9096
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2023-02-15 07:56:55 +0300

    Добавление елементов одного среза к другому

--- src/master_Go/slices/AppendAndCopy/main.go
+++ src/master_Go/slices/AppendAndCopy/main.go
@@ -7,7 +7,13 @@ func main() {
 	// Append - создает новый экземпляр среза, а не изменяет базовый!!!
 	// Поэтому нужно снова привязывать к той же переменной
 	numbers := []float64{1.0, 4.5}
+	fmt.Printf("Adress slice: %p\n", &numbers)
 	numbers = append(numbers, 18.0)
 	fmt.Println(numbers)
+	fmt.Printf("Adress slice: %p\n", &numbers) // Адресс остался тот же
 
+	// Добавление елементов одного среза к другому
+	slice := []float64{10.5, 22.8}
+	numbers = append(numbers, slice...)
+	fmt.Println(numbers)
 }
