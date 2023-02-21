e2725423c2e4b8b290faed85d34658629e19a6eb
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2023-02-21 10:22:52 +0300

    Strings. First commit

---
+++ src/master_Go/strings/lec2/main.go
@@ -0,0 +1,8 @@
+// Runes, Bytes and Unicode
+package main
+
+func main() {
+	// byte (alias by uint8) - this is ASCII, rune(aliase by int32) - this is Point Unicode
+	// rune - тип по умолчанию - для отдельного символа
+
+}
