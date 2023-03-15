231a7bed7c373a21cd7430afb05bd7c4da7b9a1e
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2023-03-15 15:26:58 +0300

    / Записываев файл. Способ 1.

--- src/master_Go/files/main.go
+++ src/master_Go/files/main.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 )
@@ -18,6 +19,48 @@ func main() {
 
 	// Очистка или обрезка файла
 	err = os.Truncate("test.txt", 22) // Обрезать до 22 символов.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Открытие, закрытие файла
+	file, err := os.OpenFile("test.txt", os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatal()
+	}
+	file.Close()
+
+	// Информация о файле
+	var fileInfo os.FileInfo
+	fileInfo, err = os.Stat("test.txt")
+	p := fmt.Println
+	p("File name: ", fileInfo.Name())        // Имя
+	p("Size of bytes: ", fileInfo.Size())    // Размер файла
+	p("Это директория?: ", fileInfo.IsDir()) // Директория
+	p("Разрешения: ", fileInfo.Mode())       // Разрешения
+
+	// проверка на существование файла
+	fileInfo, err = os.Stat("a.txt")
+	if err != nil {
+		if os.IsNotExist(err) {
+			fmt.Println("Файл не существует!")
+		}
+	}
+
+	// Переименование файла
+	oldPath := "test.txt"
+	newPath := "newText.doc"
+	err = os.Rename(oldPath, newPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Удаление файла
+	err = os.Remove("newText.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = os.Remove("newText.doc")
 	if err != nil {
 		log.Fatal(err)
 	}
---
+++ src/master_Go/files/writingBytes/main.go
@@ -0,0 +1,27 @@
+// Запись фрагмента байт в файл
+package main
+
+import (
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.OpenFile(
+		"a.txt",
+		os.O_WRONLY|os.O_TRUNC|os.O_CREATE,
+		0644,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// Записываем в файл. Способ 1.
+	bytSlice := []byte("Этот текст был записан из программы на Go!")
+	bytWritten, err := file.Write(bytSlice)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Записано байт: %d\n", bytWritten)
+}
