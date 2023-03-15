c8693f4cefdc743db41f75b592b0aeba48482281
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2023-03-15 10:26:32 +0300

    ФалыÑ. Продолжение.

--- src/master_Go/files/main.go
+++ src/master_Go/files/main.go
@@ -2,25 +2,24 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 )
 
 func main() {
-	pf := fmt.Printf
-
-	var newFile *os.File
-	pf("%T\n", newFile)
 
 	var err error
-	newFile, err = os.Create("test.txt")
+	newFile, err := os.Create("test.txt")
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
 	}
 	newFile.Close()
 
-	// Очистка файла
-	err = os.Truncate("test.txt", 0)
+	// Очистка или обрезка файла
+	err = os.Truncate("test.txt", 22) // Обрезать до 22 символов.
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
