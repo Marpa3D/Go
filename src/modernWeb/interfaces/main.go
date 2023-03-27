c2ebbdc3b3398320fdb685b036b4c53d725e8276
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2023-03-27 08:42:38 +0300

    Реализация интерфейса

--- src/modernWeb/interfaces/main.go
+++ src/modernWeb/interfaces/main.go
@@ -1,6 +1,55 @@
 // Работа с интерфейсами
 package main
 
+import "log"
+
+type Animal interface {
+	Says() string
+	NumberOfPets() int
+}
+
+type Cat struct {
+	Name  string
+	Breed string
+}
+
+type Gorilla struct {
+	Name       string
+	Color      string
+	NumOfTeeth int
+}
+
 func main() {
 
+	cat := Cat{
+		Name:  "Bob",
+		Breed: "Kamyshovaya",
+	}
+	PrintInfo(cat)
+
+	gor := Gorilla{
+		Name:       "BigFoot",
+		Color:      "Black",
+		NumOfTeeth: 34,
+	}
+
+	PrintInfo(gor)
+}
+
+func (c Cat) Says() string {
+	return "Мяу!)"
+}
+func (c Cat) NumberOfPets() int {
+	return 4
+}
+
+func (g Gorilla) Says() string {
+	return "Арр!"
+}
+func (g Gorilla) NumberOfPets() int {
+	return 4
+}
+
+func PrintInfo(a Animal) {
+	log.Println("Это животное говорит:", a.Says(), "У него лапок:", a.NumberOfPets())
 }
