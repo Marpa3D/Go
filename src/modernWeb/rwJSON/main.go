0c2d72a8362da45c90090672ca0a3052e81b711d
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2023-04-18 08:03:06 +0300

    Преобразование JSON в структуру

--- src/modernWeb/rwJSON/main.go
+++ src/modernWeb/rwJSON/main.go
@@ -1,6 +1,18 @@
 // Чтение и запись JSON
 package main
 
+import (
+	"encoding/json"
+	"log"
+)
+
+type Person struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Color     string `json:"color"`
+	HasCat    bool   `json:"has_cat"`
+}
+
 func main() {
 
 	myJson := `
@@ -16,7 +28,15 @@ func main() {
 		"last_name": "Malkovich",
 		"color": "red",
 		"has_cat": false
-	},
+	}
 ]`
+	// Преобразование формата JSON в структуру
+	var unmarshalled []Person
 
+	err := json.Unmarshal([]byte(myJson), &unmarshalled)
+
+	if err != nil {
+		log.Println("Формат не соответствует JSON", err)
+	}
+	log.Printf("unmarshalled: %v\n", unmarshalled)
 }
