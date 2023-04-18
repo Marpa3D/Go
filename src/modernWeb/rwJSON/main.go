c6dd15c7e2de084bd52698afde10f16bdc3ae86c
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2023-04-18 07:46:21 +0300

    JSON. First commit

---
+++ src/modernWeb/rwJSON/main.go
@@ -0,0 +1,22 @@
+// Чтение и запись JSON
+package main
+
+func main() {
+
+	myJson := `
+[
+	{
+		"first_name": "Mark",
+		"last_name": "Wolfman",
+		"color": "white",
+		"has_cat": true
+	},
+	{
+		"first_name": "Jhon",
+		"last_name": "Malkovich",
+		"color": "red",
+		"has_cat": false
+	},
+]`
+
+}
