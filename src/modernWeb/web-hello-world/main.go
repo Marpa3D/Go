0c85915020b1cec7e6f3d24de869c54f2f7d7386
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2023-04-20 13:58:07 +0300

    Error checked

--- src/modernWeb/web-hello-world/main.go
+++ src/modernWeb/web-hello-world/main.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -24,10 +25,28 @@ func About(w http.ResponseWriter, r *http.Request) {
 	_, _ = fmt.Fprintf(w, "<h1>Это страница \"о нас\"</h1>")
 }
 
+//Divide - обработчик функции деления
+func Divide(w http.ResponseWriter, r *http.Request) {
+	v, err := divideValues(200.0, 20.0)
+	if err != nil {
+		fmt.Fprintf(w, "Ошибка деления на ноль.")
+	}
+	fmt.Fprintf(w, "%f делить на %f = %f", 200.0, 20.0, v)
+}
+func divideValues(x, y float64) (float64, error) {
+	if y == 0 {
+		err := errors.New("Нельзя делить на ноль!")
+		return 0, err
+	}
+	res := x / y
+	return res, nil
+}
+
 // main - точка входа в приложение
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 	fmt.Println("Сервер запускается...")
 	_ = http.ListenAndServe(portNumber, nil)
 
