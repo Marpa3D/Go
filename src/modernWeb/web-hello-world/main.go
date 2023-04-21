9ce0cae61911c71dc3f10c6f2c37b39ced4496e9
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2023-04-21 13:05:57 +0300

    Созданы два статических шаблона и их рендер по марщрутам

--- src/modernWeb/web-hello-world/main.go
+++ src/modernWeb/web-hello-world/main.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
@@ -10,12 +11,23 @@ const portNumber string = ":8080"
 
 // Home функция обработчик страницы "Главная"
 func Home(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "home.page.tmpl")
 
 }
 
 // About - функция обработчик страницы "О нас"
 func About(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "about.page.tmpl")
+}
 
+//rengerTemplate - функция рендера шаблона по имени
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parseTmpl, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parseTmpl.Execute(w, nil)
+	if err != nil {
+		fmt.Println("Ошибка парсинга шаблона", err)
+		//return
+	}
 }
 
 // main - точка входа в приложение
@@ -23,7 +35,7 @@ func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
 
-	fmt.Println("Сервер запускается...")
+	fmt.Println("Сервер запускается в порту:8080")
 	_ = http.ListenAndServe(portNumber, nil)
 
 }
