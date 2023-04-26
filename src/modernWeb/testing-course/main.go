1053245fa1e10c4b7800303f7c7a2aec53941116
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2023-04-26 13:48:28 +0300

    Go and testing. First commit

--- src/modernWeb/testing-course/main.go
+++ src/modernWeb/testing-course/main.go
@@ -1,26 +1,12 @@
 // Тестирование
 package main
 
-import (
-	"errors"
-	"log"
-)
+import "fmt"
 
-func main() {
-	res, err := divide(100.0, 10.0)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	log.Println("Результат деления:", res)
+func Hello(name string) string {
+	return "Hello " + name
 }
 
-func divide(x, y float64) (float64, error) {
-	var result float64
-
-	if y == 0 {
-		return result, errors.New("Ошибка деления на ноль!")
-	}
-	result = x / y
-	return result, nil
+func main() {
+	fmt.Println(Hello("Mark"))
 }
--- src/modernWeb/testing-course/main_test.go
+++ src/modernWeb/testing-course/main_test.go
@@ -3,32 +3,11 @@ package main
 
 import "testing"
 
-// Табличный тест
-var tests = []struct {
-	name     string
-	divident float64
-	divisor  float64
-	expected float64
-	isErr    bool
-}{
-	{"valid-data", 100.0, 10.0, 10.0, false},
-	{"invalid-data", 100.0, 0.0, 0.0, true},
-}
+func TestHello(t *testing.T) {
+	got := Hello("Mark")
+	want := "Hello Mark"
 
-func TestDivision(t *testing.T) {
-	for _, tt := range tests {
-		got, err := divide(tt.divident, tt.divisor)
-		if tt.isErr {
-			if err == nil {
-				t.Error("Ожидали ошибку, но не получили")
-			}
-		} else {
-			if err != nil {
-				t.Error("Не ожидали ошибку, но получили ее", err.Error())
-			}
-		}
-		if got != tt.expected {
-			t.Errorf("Ожидали %f, но получили %f", tt.expected, got)
-		}
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
 	}
 }
