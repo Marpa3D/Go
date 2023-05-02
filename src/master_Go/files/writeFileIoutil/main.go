d31f1526a67bd948c9e117ac6b207d7115fc958e
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2023-05-02 08:13:02 +0300

    ÐСоздан файÐЗапись строÐСоздание файла и ºи в файл через os.OpenFile

--- src/master_Go/files/writeFileIoutil/main.go
+++ src/master_Go/files/writeFileIoutil/main.go
@@ -1,6 +1,28 @@
 // Запись байт в файл. os Write and ioutil write File
 package main
 
-func main() {
+import (
+	"log"
+	"os"
+)
 
+func main() {
+	file, err := os.OpenFile(
+		"a.txt", os.O_WRONLY|os.O_TRUNC|os.O_CREATE,
+		0644,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// Запись слайса байт(он изменяем, в отличии от строки)
+	byteSlice := []byte("Каждый может тебя чему то научить!")
+	byteWritten, err := file.Write(byteSlice)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Колличество записанных байт: %d\n ", byteWritten)
 }
