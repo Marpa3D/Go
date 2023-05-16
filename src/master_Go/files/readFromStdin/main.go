78dd368398557499ff91f70df57fcccebde2011a
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2023-05-16 08:04:23 +0300

    Считывание, пока не введена команда exit

--- src/master_Go/files/readFromStdin/main.go
+++ src/master_Go/files/readFromStdin/main.go
@@ -19,4 +19,14 @@ func main() {
 
 	fmt.Println("Введеный текст:", text)
 	fmt.Println("Прочитанные байты:", bytes)
+
+	// Считывание, пока не введена команда "exit"
+	for scanner.Scan() {
+		text := scanner.Text()
+		fmt.Println("Вы ввели текст:", text)
+		if text == "exit" {
+			fmt.Println("Выход из сканирования...")
+			break
+		}
+	}
 }
