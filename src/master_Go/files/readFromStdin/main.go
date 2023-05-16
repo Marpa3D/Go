8a8048dd283045cdfb13c4720255b217efe4caf4
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2023-05-16 07:54:58 +0300

    ÐСоздан сканнер из стандартного ввода Stdin

--- src/master_Go/files/readFromStdin/main.go
+++ src/master_Go/files/readFromStdin/main.go
@@ -1,6 +1,22 @@
-// Считывание пользовательского ввода из Stdin
+// Считывание пользовательского ввода из стандартного ввода Stdin
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf("%T\n", scanner)
+
+	// Сканируй, санер!)
+	scanner.Scan()
+
+	text := scanner.Text()
+	bytes := scanner.Bytes()
+
+	fmt.Println("Введеный текст:", text)
+	fmt.Println("Прочитанные байты:", bytes)
 }
