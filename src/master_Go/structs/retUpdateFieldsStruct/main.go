68d71f875248c569411d5fcebdf27d17831a38e7
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2023-05-18 08:00:19 +0300

    Сравнение структур

--- src/master_Go/structs/retUpdateFieldsStruct/main.go
+++ src/master_Go/structs/retUpdateFieldsStruct/main.go
@@ -1,7 +1,10 @@
 // Извлечение и обновление полей структуры
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type book struct {
 	title  string
@@ -10,14 +13,22 @@ type book struct {
 }
 
 func main() {
-	newBook := book{title: "Грокаем алгориты"}
-	fmt.Printf("\"%v\"\n", newBook.title)
+	myBook := book{title: "Грокаем алгориты"}
+	fmt.Printf("\"%v\"\n", myBook.title)
 
 	// Печать типа и полей - верба (глагол) #v
-	fmt.Printf("%#v\n", newBook)
+	fmt.Printf("%#v\n", myBook)
 
 	// Печать полей структуры и их значений
-	newBook.author = "Indi"
-	newBook.year = 2013
-	fmt.Printf("%+v", newBook)
+	myBook.author = "Indi"
+	myBook.year = 2013
+	fmt.Printf("%+v\n", myBook)
+
+	// Сравнение структур. Сравнивать можно (==, !=).
+	// Структуры равны, если их соотвествующие поля равны!
+	newBook := book{title: "Грокаем алгориты", author: "Indi", year: 2013}
+
+	fmt.Println(strings.Repeat("*", 54))
+	fmt.Println("Равны ли две структуры?)")
+	fmt.Println(myBook == newBook)
 }
