017c65033e6f4382898b204ad1fb9b44324716ea
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2023-05-19 08:56:11 +0300

    ÐÐСтруткуры и практика по ним

--- src/master_Go/structs/anonymousStructs/main.go
+++ src/master_Go/structs/anonymousStructs/main.go
@@ -1,7 +1,45 @@
 // Анонимные структуры и поля
 package main
 
+import "fmt"
+
 func main() {
-	// Анонимная структура - это структура, которая не имеет имени (явного псеводонима типа структуры).
+	// 1. Анонимная структура - это структура, которая не имеет имени (явного псеводонима типа структуры).
+	// Полезно, когда не нужно повторно использовать структуру в проекте!
+	aStruct := struct {
+		firstName string
+		lastName  string
+		age       int
+	}{
+		firstName: "Mark",
+		lastName:  "Evangelist",
+		age:       98,
+	}
+	fmt.Printf("Поля анонимной струкутры: %#v\n", aStruct)
+	fmt.Printf("Возраст: %#v лет.\n", aStruct.age)
+
+	// 2. Анонимные поля структуры - поля без именб только типы данных
+	type Book struct {
+		string
+		float64
+		int
+	}
+	b := Book{"M.Plank", 22.8, 98}
+	fmt.Printf("Values fields  struct variable b: %#v\n", b)
+
+	// Вывод поля
+	fmt.Printf("%v\n", b.string)
+
+	// 3. Смешивание именованных и анонимных полей
+	type Employee struct {
+		name   string
+		salary float64
+		bool   // анонимное поле
+	}
+	e := Employee{"Jhon", 45000.0, false}
+	fmt.Printf("%#v\n", e)
+
+	e.bool = true
+	fmt.Printf("%#v\n", e)
 
 }
