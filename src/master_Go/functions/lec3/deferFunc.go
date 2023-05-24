f1f2dd2cfcd586ea98c46bc945c614e28377ed97
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2023-05-24 07:33:20 +0300

    Anonymous func. First commit

--- src/master_Go/functions/lec3/deferFunc.go
+++ src/master_Go/functions/lec3/deferFunc.go
@@ -1,9 +1,38 @@
 // defer - оператор отсрочки
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func foo() {
+	fmt.Println("This is func foo()")
+}
+
+func bar() {
+	fmt.Println("This is func bar()")
+}
+
+func fooBar() {
+	fmt.Println("This is func fooBar()")
+}
+
 func main() {
 	// defer - откладывает выполнение функции пока текущая функция не завершится
 	// он помещает вызов указанной функции в список вызовов,
 	//который выполняется после завершения работы окружающей функции
+	defer foo()
+	bar()
+	fmt.Println("Текст после вызовов foo() и bar()")
+	defer fooBar() // defer помещает йункции в стек.
+	// LIFO (последний пришел, первый ушел)
+
+	file, err := os.Open("main.go")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
 }
